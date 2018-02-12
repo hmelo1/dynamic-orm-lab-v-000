ad9a4d6ed44b94e1c79bc24023b7826cff2a53f9
Eddie Melo <heriberto@melo.nyc>
2018-02-12 00:24:07 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,14 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  # self = student
+  # Student to string to lowercase to "students"
   def self.table_name
     self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+
   end
 end
